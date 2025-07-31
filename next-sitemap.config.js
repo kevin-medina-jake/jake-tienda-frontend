504941cf7531adb94f762a82b5c18186cff5b6c0
Filollan <mllanten@unimayor.edu.co>
2025-07-31 16:57:29 -0500

    Implement code changes to enhance functionality and improve performance

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+ // siteUrl: 'http://localhost:3000', // Cámbialo cuando tengas tu dominio final
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
