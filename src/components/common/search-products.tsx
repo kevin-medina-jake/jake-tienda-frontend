05f22374e268386fc7f1ddea4d7e5c3e6c35a4ef
igrisdev <johanalvarezz237@gmail.com>
2025-08-04 14:53:45 -0500

    mejora

--- src/components/common/search-products.tsx
+++ src/components/common/search-products.tsx
@@ -3,41 +3,84 @@
 import { useFilterProducts } from "@/hooks/use-filter-products";
 import { useStoreProducts } from "@/store/products";
 import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect } from "react";
 
 export const SearchProducts = () => {
   const { handleSearch } = useFilterProducts();
-  const { filters } = useStoreProducts();
+  const { filters, filteredProducts } = useStoreProducts();
   const { search } = filters;
 
   const pathname = usePathname();
   const router = useRouter();
 
-  const [wasEmpty, setWasEmpty] = useState(true);
+  const wasEmpty: boolean = search === "";
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
 
-    if (wasEmpty && value.trim() !== "" && pathname !== "/products") {
-      router.push("/products");
-    }
-
-    setWasEmpty(value.trim() === "");
-
     handleSearch({ search: value });
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      router.push("/products");
+    }
+  };
+
+  useEffect(() => {
+    if (pathname === "/products") return;
+
+    handleSearch({ search: "" });
+  }, [pathname]);
+
   return (
-    <div className="relative flex w-full items-center">
-      <input
-        type="text"
-        className="w-full rounded-full border border-gray-400 px-4 py-2 focus:border-blue-50"
-        placeholder="Buscar..."
-        value={search}
-        onChange={handleChange}
-      />
-      <Search className="absolute right-4" />
-    </div>
+    <section className="relative">
+      <div className="relative flex w-full items-center">
+        <input
+          type="text"
+          className="w-full rounded-full border border-gray-400 px-4 py-2 focus:border-blue-50"
+          placeholder="Buscar..."
+          value={search}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <Search className="absolute right-4" />
+      </div>
+
+      {filteredProducts.length > 0 &&
+        wasEmpty !== true &&
+        pathname !== "/products" && (
+          <ul className="absolute top-[100%] flex max-h-96 w-full flex-col gap-2 overflow-y-auto rounded-sm bg-blue-50">
+            {filteredProducts.map((product) => (
+              <li
+                key={product.id}
+                className={pathname.includes(product.slug) ? "hidden" : "p-2"}
+              >
+                <Link
+                  href={`/view-product/${product.slug}`}
+                  className="flex items-center gap-2 rounded-xs border border-gray-300 p-2 hover:bg-blue-100"
+                >
+                  <div>
+                    <Image
+                      src={product.image || "/not-found.png"}
+                      alt="logo"
+                      width={50}
+                      height={50}
+                    />
+                  </div>
+
+                  <p>
+                    {product.categories.map((category) => category)} -
+                    {product.brand} - {product.name}
+                  </p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+    </section>
   );
 };
