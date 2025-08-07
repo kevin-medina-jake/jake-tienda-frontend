8eb7c8c247533667c28ba62f4b76aad055e3a4bd
igrisdev <johanalvarezz237@gmail.com>
2025-08-06 19:23:27 -0500

    mejora diseño pay process

--- src/app/pay/process/page.tsx
+++ src/app/pay/process/page.tsx
@@ -5,22 +5,24 @@ import { Circle, CircleCheckIcon } from "lucide-react";
 export default function Page() {
   return (
     <div className="mx-auto flex min-h-screen max-w-7xl flex-col gap-10 px-4">
-      <section className="flex items-center justify-evenly pt-12 text-lg">
-        <div className="flex items-center justify-center gap-2 text-blue-500">
+      <section className="flex flex-wrap items-center justify-evenly gap-2 pt-12 text-lg">
+        <div className="flex items-center justify-center gap-2 whitespace-nowrap text-blue-500">
           <CircleCheckIcon size={24} />
           <p>Finalizar Compra</p>
         </div>
 
-        <div className="flex items-center justify-center gap-2 text-gray-500">
+        <div className="flex items-center justify-center gap-2 whitespace-nowrap text-gray-500">
           <Circle size={24} />
           <p>Resultado de la compra</p>
         </div>
       </section>
 
-      <section className="grid grid-cols-2 gap-8">
+      <section className="grid gap-8 md:grid-cols-2">
         <FormPayProducts />
 
-        <InfoShoppingCart />
+        <div className="row-start-1 md:col-start-2">
+          <InfoShoppingCart />
+        </div>
       </section>
     </div>
   );
