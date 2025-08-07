5de5394cb6bbd7825cf05315e972be63d98e8498
igrisdev <johanalvarezz237@gmail.com>
2025-08-07 12:24:09 -0500

    mejora del buscador

--- src/app/api/strapi/search/route.ts
+++ src/app/api/strapi/search/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-
 import { client } from "@/service/api/strapi";
 import { parseProductCart } from "@/lib/parse/parse-products";
 
@@ -8,32 +7,40 @@ export async function GET(request: Request) {
   const query = searchParams.get("q") || "";
 
   try {
-    let response;
-    if (!query) {
-      response = await client.collection("products").find({
-        populate: ["brand", "categories", "images"],
-        status: "published",
-        sort: "createdAt:desc",
-      });
-    } else {
-      response = await client.collection("products").find({
-        filters: {
-          $or: [
-            { name: { $containsi: query } },
-            { brand: { name: { $containsi: query } } },
-            { categories: { name: { $containsi: query } } },
-          ],
-        },
-        populate: ["brand", "categories", "images"],
-        status: "published",
-        sort: "createdAt:desc",
-      });
+    let filters = {};
+
+    if (query) {
+      // 1. Divide la cadena de búsqueda en palabras
+      const searchWords = query.toLowerCase().split(" ").filter(Boolean);
+
+      // 2. Construye los filtros dinámicamente
+      if (searchWords.length > 0) {
+        // Usa $and para que el producto deba coincidir con todas las palabras clave
+        filters = {
+          $and: searchWords.map((word) => ({
+            $or: [
+              { name: { $containsi: word } },
+              { brand: { name: { $containsi: word } } },
+              { categories: { name: { $containsi: word } } },
+            ],
+          })),
+        };
+      }
     }
+
+    // 3. Realiza la llamada a Strapi con los filtros construidos
+    const response = await client.collection("products").find({
+      filters: filters, // Pasa el objeto de filtros dinámico
+      populate: ["brand", "categories", "images"],
+      status: "published",
+      sort: "createdAt:desc",
+    });
 
     const result = parseProductCart(response.data);
 
     return NextResponse.json(result);
   } catch (error) {
+    console.error("Error en la búsqueda de productos:", error);
     return new NextResponse("Server Error", { status: 500 });
   }
 }
