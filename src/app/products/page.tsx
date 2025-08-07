6f303e21aff61f6ab85a223f5148d7dc23aadb9b
igrisdev <johanalvarezz237@gmail.com>
2025-08-07 10:22:56 -0500

    quitanto de page toda la logica

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,50 +1,5 @@
-"use client";
+import ViewProducts from "@/components/products/view-products";
 
-import { Suspense } from "react";
-import ProductsPage from "@/components/products/products-page";
-import { useFilterProducts } from "@/hooks/use-filter-products";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import { useStoreProducts } from "@/store/products";
-import { getFilterProducts } from "@/service/api/product";
-
-function ProductsContent() {
-  // const { handleCategories, handleBrands } = useFilterProducts();
-  const { setAllProducts, filters, setLoading, allProducts } =
-    useStoreProducts();
-
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    if (filters.search.length > 0 && allProducts.length > 0) return;
-
-    setLoading(true);
-    const page = searchParams.get("page");
-    const currentPage = page && !isNaN(Number(page)) ? Number(page) : 1;
-
-    getFilterProducts({ page: currentPage }).then((productsFilter) => {
-      setLoading(false);
-      setAllProducts(productsFilter);
-    });
-  }, []);
-
-  // useEffect(() => {
-  //   const category = searchParams.get("category");
-  //   if (category) handleCategories({ categories: category.split(",") });
-  // }, [searchParams]);
-
-  // useEffect(() => {
-  //   const brand = searchParams.get("brand");
-  //   if (brand) handleBrands({ brands: brand.split(",") });
-  // }, [searchParams]);
-
-  return <ProductsPage />;
-}
-
-export default function Products() {
-  return (
-    <Suspense fallback={<div></div>}>
-      <ProductsContent />
-    </Suspense>
-  );
+export default function Page() {
+  return <ViewProducts />;
 }
---
+++ src/components/products/view-products.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Suspense } from "react";
+import ProductsPage from "@/components/products/products-page";
+import { useFilterProducts } from "@/hooks/use-filter-products";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { useStoreProducts } from "@/store/products";
+import { getFilterProducts } from "@/service/api/product";
+
+function ProductsContent() {
+  // const { handleCategories, handleBrands } = useFilterProducts();
+  const { setAllProducts, filters, setLoading, allProducts } =
+    useStoreProducts();
+
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (filters.search.length > 0 && allProducts.length > 0) return;
+
+    setLoading(true);
+    const page = searchParams.get("page");
+    const currentPage = page && !isNaN(Number(page)) ? Number(page) : 1;
+
+    getFilterProducts({ page: currentPage }).then((productsFilter) => {
+      setLoading(false);
+      setAllProducts(productsFilter);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   const category = searchParams.get("category");
+  //   if (category) handleCategories({ categories: category.split(",") });
+  // }, [searchParams]);
+
+  // useEffect(() => {
+  //   const brand = searchParams.get("brand");
+  //   if (brand) handleBrands({ brands: brand.split(",") });
+  // }, [searchParams]);
+
+  return <ProductsPage />;
+}
+
+export default function ViewProducts() {
+  return (
+    <Suspense fallback={<div></div>}>
+      <ProductsContent />
+    </Suspense>
+  );
+}
