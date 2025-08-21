82bee21d21d8115245875794c18aeb6549917645
igrisdev <johanalvarezz237@gmail.com>
2025-08-20 22:03:47 -0500

    mejora 2

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -490,10 +490,9 @@ export async function addToCart(
   return reshapeCart(res.body.data.cartLinesAdd.cart);
 }
 
-// This is called from `app/api/revalidate.ts` so providers can control revalidation logic.
 export async function revalidate(req: NextRequest): Promise<NextResponse> {
-  // We always need to respond with a 200 status code to Shopify,
-  // otherwise it will continue to retry the request.
+  const topic = req.headers.get("x-shopify-topic") || "unknown";
+  const secret = req.nextUrl.searchParams.get("secret");
 
   const collectionWebhooks = [
     "collections/create",
@@ -505,15 +504,12 @@ export async function revalidate(req: NextRequest): Promise<NextResponse> {
     "products/delete",
     "products/update",
   ];
-
   const metaobjectWebhooks = [
     "metaobjects/create",
     "metaobjects/update",
     "metaobjects/delete",
   ];
 
-  const topic = headers().get("x-shopify-topic") || "unknown";
-  const secret = req.nextUrl.searchParams.get("secret");
   const isCollectionUpdate = collectionWebhooks.includes(topic);
   const isProductUpdate = productWebhooks.includes(topic);
   const isMetaobjectUpdate = metaobjectWebhooks.includes(topic);
@@ -523,25 +519,75 @@ export async function revalidate(req: NextRequest): Promise<NextResponse> {
     return NextResponse.json({ status: 200 });
   }
 
-  if (!isCollectionUpdate && !isProductUpdate) {
-    // We don't need to revalidate anything for any other topics.
+  if (!isCollectionUpdate && !isProductUpdate && !isMetaobjectUpdate) {
     return NextResponse.json({ status: 200 });
   }
 
   if (isCollectionUpdate) {
     revalidateTag(TAGS.collections);
   }
-
   if (isProductUpdate) {
     revalidateTag(TAGS.products);
   }
-
   if (isMetaobjectUpdate) {
     revalidateTag(TAGS.metaobjects);
   }
 
   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
 }
+
+// // This is called from `app/api/revalidate.ts` so providers can control revalidation logic.
+// export async function revalidate(req: NextRequest): Promise<NextResponse> {
+//   // We always need to respond with a 200 status code to Shopify,
+//   // otherwise it will continue to retry the request.
+
+//   const collectionWebhooks = [
+//     "collections/create",
+//     "collections/delete",
+//     "collections/update",
+//   ];
+//   const productWebhooks = [
+//     "products/create",
+//     "products/delete",
+//     "products/update",
+//   ];
+
+//   const metaobjectWebhooks = [
+//     "metaobjects/create",
+//     "metaobjects/update",
+//     "metaobjects/delete",
+//   ];
+
+//   const topic = headers().get("x-shopify-topic") || "unknown";
+//   const secret = req.nextUrl.searchParams.get("secret");
+//   const isCollectionUpdate = collectionWebhooks.includes(topic);
+//   const isProductUpdate = productWebhooks.includes(topic);
+//   const isMetaobjectUpdate = metaobjectWebhooks.includes(topic);
+
+//   if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
+//     console.error("Invalid revalidation secret.");
+//     return NextResponse.json({ status: 200 });
+//   }
+
+//   if (!isCollectionUpdate && !isProductUpdate) {
+//     // We don't need to revalidate anything for any other topics.
+//     return NextResponse.json({ status: 200 });
+//   }
+
+//   if (isCollectionUpdate) {
+//     revalidateTag(TAGS.collections);
+//   }
+
+//   if (isProductUpdate) {
+//     revalidateTag(TAGS.products);
+//   }
+
+//   if (isMetaobjectUpdate) {
+//     revalidateTag(TAGS.metaobjects);
+//   }
+
+//   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
+// }
 
 export async function getPage(handle: string): Promise<Page> {
   const res = await shopifyFetch<ShopifyPageOperation>({
