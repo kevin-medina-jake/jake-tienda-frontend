57066ad23d5d31290b2f8f51bcd7a3cf5aa125fc
igrisdev <johanalvarezz237@gmail.com>
2025-08-29 19:43:15 -0500

    refactor: improve price formatting logic in Price component

--- src/components/price.tsx
+++ src/components/price.tsx
@@ -12,11 +12,15 @@ const Price = ({
   currencyCodeClassName?: string;
 } & React.ComponentProps<"p">) => (
   <p suppressHydrationWarning={true} className={className}>
-    {`${new Intl.NumberFormat(undefined, {
-      style: "currency",
-      currency: currencyCode,
-      currencyDisplay: "narrowSymbol",
-    }).format(parseFloat(amount))}`}
+    {`${
+      new Intl.NumberFormat(undefined, {
+        style: "currency",
+        currency: currencyCode,
+        currencyDisplay: "narrowSymbol",
+      })
+        .format(parseFloat(amount))
+        .split(",")[0]
+    }`}
     <span
       className={clsx("ml-1 inline", currencyCodeClassName)}
     >{`${currencyCode}`}</span>
